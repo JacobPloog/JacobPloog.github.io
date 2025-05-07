255487ad746959203ecf9dc73743755967d2ec67
Jacob Ploog <jploog066@west-mec.org>
2025-05-07 14:24:52 -0700

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 import { useState } from "react";
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
         } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home/>
       </div>
     </>
   );
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return <section id='home' className="min-h-screen flex items-center justify-center relative">
+        <div className="test-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 gradient-text leading-right">
+                Hi, I'm Jake Ploog
+            </h1>
+        </div>
+    </section>
+}
